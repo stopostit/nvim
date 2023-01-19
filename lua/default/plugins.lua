c4939a17e3484d8bb4010267fcd1717e20df3572
Maxime CAISEZ <maxime@scalingo.com>
2023-01-19 08:56:28 +0100

    fix(lsp handlers): update config to remove deprecated method

--- lua/default/plugins.lua
+++ lua/default/plugins.lua
@@ -42,7 +42,6 @@ packer.init {
 return packer.startup(function(use)
 	-- My plugins here
 	use "wbthomason/packer.nvim" -- Have packer manage itself
-	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 	use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
 
 	-- Colorschemes
